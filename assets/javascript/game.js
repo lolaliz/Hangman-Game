31f6d478a1844df354dfbdaf6d455448adfe79e0
lolaliz <34426527+lolaliz@users.noreply.github.com>
2018-02-16 20:02:51 -0800

    latest changes, some roadblocks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,56 +1,154 @@
-//create an array of South African animals
+//create an array of Australian animals
 var AusAnimals = ['kookaburra', 'wombat', 'platypus', 'wallaby', 'echidna', 'quokka', 'dingo', 'bilby', 'crocodile']; 
+//valid characters accepted
+var validLetters = ["a", 'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
 var wins = 0;
 var losses = 0;
-var underscores = [];
+//holds number of underscores
+var underscores = 0;
+var lettersinAnim = [];
 var userGuess = [];
 var wGuesses = [];
-var CorrGuesses = [];
-var guessesL = [];
+//holds underscores and correct guesses
+var CorrGuessesandUnders = [];
+var guessesL = 0;
+var counterCorrGuesses = 0;
 
-//create event listener to get game started
-document.onkeyup = function () { 
-//function: computer chooses word from animal array randomly when prompted by first event listener
+
+
+
+function updateScore(){
+    if (updateWin()){ 
+        wins++;
+        document.getElementById("win-counter").innerHTML = wins;   
+
+}
+    else (updateLoss());{
+    
+        losses++;
+        document.getElementById("loss-conter").innerHTML = losses;
+        
+        
+
+    }
+}
+
+function updateWin (underscores = lettersinAnim.length){
+    
+    reset();
+    //add what happens if the user wins code here
+
+    //Make a call to the startGame function here. 
+    //This call is here to start the game once a player wins.
+
+}
+
+function updateLoss(guessesL= 0) {
+   
+    reset();
+    //add what happens if the user loses code here
+
+    //Make a call to the startGame function here.
+    //This call is here to start the game once a player losses.
+
+}
+
+
+
+
+function reset() 
+{
+var randAnim = AusAnimals [Math.floor(Math.random() * AusAnimals.length)];
+console.log('random word: ' + randAnim);
+var lettersinAnim = randAnim.split('');
+underscores = lettersinAnim.length;
+
+wGuesses = [];
+guessesL = 10;
+
+for(var i = 0; i< underscores; i++)
+    {
+        CorrGuessesandUnders.push('_');
+        document.getElementById('word-blanks').innerHTML = CorrGuessesandUnders;
+    }
+document.getElementById('word-blanks').innerHTML = CorrGuessesandUnders.join(" ");
+document.getElementById("guesses-left").innerHTML = guessesL 
+}
+
+
+
+
+//create function to get game started
+document.onkeyup = function gameStart () { 
+//computer chooses word from animal array randomly when prompted by first event listener
 //function determines length of word chosen by computer
 var randAnim = AusAnimals [Math.floor(Math.random() * AusAnimals.length)];
 console.log('random word: ' + randAnim);
-//var wordInplay = AusAnimals [randAnim];
-//console.log(randAnim.length);
-
+//splits the randomly chosen word into array of letters
+var lettersinAnim = randAnim.split('');
+//get the number of underscores/word-blanks
+underscores = lettersinAnim.length;
 //change underscore 'Word to Guess' to match word from animal array chosen randomly (id="word-blanks")
 
-for(var i = 0; i < randAnim.length; i++){
-    underscores.push ('_');
-}
-//print 'word-blanks' to screen
-document.getElementById("word-blanks").innerHTML = underscores.join(" ");
 //set beginning of game wrong guesses and guesses left
-wGuess = [];
+wGuesses = [];
 guessesL = 10;
 
-document.getElementById("guesses-left").innerHTML = guessesL;
+for(var i = 0; i< underscores; i++)
+	{
+		CorrGuessesandUnders.push('_');
+		document.getElementById('word-blanks').innerHTML = CorrGuessesandUnders;
+	}
+document.getElementById('word-blanks').innerHTML = CorrGuessesandUnders.join(" ");
+document.getElementById("guesses-left").innerHTML = guessesL
+
 //get/store input from user
-//if wrong letter guess display/store in Wrong Guesses "id=wrong-guesses"
-// create wrong guess array and push to end of array??
-document.onkeyup = function(event)
+document.onkeyup = function compareGuess(event)
 {
     userGuess = event.key;
     console.log (userGuess)
+    
+    if (randAnim.indexOf(userGuess) > -1) {
+         
+        for (var j=0; j<randAnim.length; j++) {
+            if (randAnim[j] === userGuess) {
+                lettersinAnim[j] = userGuess;
+                document.getElementById("word-blanks").innerHTML = lettersinAnim
+                
+                
+            }}
+        }
 
-    if(randAnim.indexOf(userGuess) > -1)
-    { CorrGuesses.push(userGuess);
-        console.log ('correct guesses:' + CorrGuesses);
+        else {
+            wGuesses.push(userGuess);
+            guessesL--;
+            document.getElementById("guesses-left").innerHTML = guessesL;
+            document.getElementById("wrong-guesses").innerHTML = wGuesses;
+            console.log ('wrong guesses:' + wGuesses);
+            updateScore();
+            
+          }
+
 
     }
-    else{
-        wGuesses.push(userGuess);
-        guessesL--;
-        console.log ('wrong guesses:' + wGuesses);
-    }
-}
+
+    
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    
+
+
+
+
 
 }
-//if correct letter guess display and replace underscore in correct spot 
-//create correct guess array to push to, display options??
-
-  
