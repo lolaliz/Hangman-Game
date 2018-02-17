c7b824502a630247c861f88e1ee2d7f86fd90079
lolaliz <34426527+lolaliz@users.noreply.github.com>
2018-02-17 02:57:08 -0800

    updates with new bugs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,21 +15,62 @@ var CorrGuessesandUnders = [];
 var guessesL = 0;
 var counterCorrGuesses = 0;
 
+function gameStart () { 
+    //computer chooses word from animal array randomly when prompted by first event listener
+    //function determines length of word chosen by computer
+    var randAnim = AusAnimals [Math.floor(Math.random() * AusAnimals.length)];
+    console.log('random word: ' + randAnim);
+    //splits the randomly chosen word into array of letters
+    var lettersinAnim = randAnim.split('');
+    //get the number of underscores/word-blanks
+    underscores = lettersinAnim.length;
+    //change underscore 'Word to Guess' to match word from animal array chosen randomly (id="word-blanks")
+    
+    //set beginning of game wrong guesses and guesses left
+    wGuesses = [];
+    guessesL = 10;
+    
+    for(var i = 0; i< underscores; i++)
+    {
+        CorrGuessesandUnders.push('_');
+        document.getElementById('word-blanks').innerHTML = CorrGuessesandUnders;
+      }
+      document.getElementById('word-blanks').innerHTML = CorrGuessesandUnders.join(" ");
+      document.getElementById("guesses-left").innerHTML = guessesL
+  }
+  
 
+function validateInput(userGuess) {
+    // Whenever a return is executed it breaks out of the function. 
+    // So if validLetters.indexOf(key) === -1 is true then i would return false. This prevents the rest of the code inside this code block from running.
+
+    // This condition Makes sure an a-z key is pressed.
+    // if not it returns a value of -1 because -1 is not a valid value
+    if(validLetters.indexOf(userGuess) === -1) return false;
+
+    // This condition Makes sure key cant be guessed again.
+    // makes sure that any indexOf value that is 0  and above (example 0=a, 1=b...) does not get used again.
+    if(wGuesses.indexOf(userGuess) >= 0) return false;
+
+    // This condition Makes sure hangman keys cant be pressed again
+    // makes sure that any indexOf value that is 0  and above (example 0=a, 1=b...) does not get used again. 
+    if(randAnim.indexOf(userGuess) >= 0) return false;
+    return true;
+}
 
 
 function updateScore(){
     if (updateWin()){ 
         wins++;
         document.getElementById("win-counter").innerHTML = wins; 
-        startGame()  
+        gameStart()  
 
 }
 else (updateLoss());{
     
         losses++;
         document.getElementById("loss-conter").innerHTML = losses;
-        startGame()
+        gameStart()
         
         
 
@@ -37,23 +78,16 @@ else (updateLoss());{
 }
 
 function updateWin (underscores = lettersinAnim.length){
+    //document.getElementById("word-blanks").innerHTML = clear "word-blanks"
+    gameStart();
     
-    startGame();
-    //add what happens if the user wins code here
-
-    //Make a call to the startGame function here. 
-    //This call is here to start the game once a player wins.
 
 }
 
 function updateLoss(guessesL= 0) {
    
-    startGame();
-    //add what happens if the user loses code here
-
-    //Make a call to the startGame function here.
-    //This call is here to start the game once a player losses.
-
+    gameStart();
+    
 }
 
 
@@ -76,45 +110,23 @@ document.onkeyup = function compareGuess(event)
             if (randAnim[j] === userGuess) {
                 CorrGuessesandUnders[j] = userGuess;
                 document.getElementById("word-blanks").innerHTML = CorrGuessesandUnders
-                updateScore()
+               // updateScore()
                 
             }}
         }
 
-        else {
+        else {//??this else function worked when I had a different function for the if??
             wGuesses.push(userGuess);
             guessesL--;
             document.getElementById("guesses-left").innerHTML = guessesL;
             document.getElementById("wrong-guesses").innerHTML = wGuesses;
             console.log ('wrong guesses:' + wGuesses);
-            updateScore();
+            //updateScore();
             
           }
 
-
-          function gameStart () { 
-            //computer chooses word from animal array randomly when prompted by first event listener
-            //function determines length of word chosen by computer
-            var randAnim = AusAnimals [Math.floor(Math.random() * AusAnimals.length)];
-            console.log('random word: ' + randAnim);
-            //splits the randomly chosen word into array of letters
-            var lettersinAnim = randAnim.split('');
-            //get the number of underscores/word-blanks
-            underscores = lettersinAnim.length;
-            //change underscore 'Word to Guess' to match word from animal array chosen randomly (id="word-blanks")
-            
-            //set beginning of game wrong guesses and guesses left
-            wGuesses = [];
-            guessesL = 10;
-            
-            for(var i = 0; i< underscores; i++)
-            {
-                CorrGuessesandUnders.push('_');
-                document.getElementById('word-blanks').innerHTML = CorrGuessesandUnders;
-              }
-              document.getElementById('word-blanks').innerHTML = CorrGuessesandUnders.join(" ");
-              document.getElementById("guesses-left").innerHTML = guessesL
-          }
+          updateScore()
+          
           
             
 
