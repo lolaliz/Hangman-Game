1f18d4886b187962a3aa1775fe6c3009c00b4104
lolaliz <34426527+lolaliz@users.noreply.github.com>
2018-02-20 15:49:54 -0800

    debugging attempt after submission

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,18 +2,19 @@
 var AusAnimals = ['kookaburra', 'wombat', 'platypus', 'wallaby', 'echidna', 'quokka', 'dingo', 'bilby', 'crocodile']; 
 //valid characters accepted
 var validLetters = ["a", 'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var randAnim = AusAnimals [Math.floor(Math.random() * AusAnimals.length)];
+var randAnim = ''//AusAnimals [Math.floor(Math.random() * AusAnimals.length)];
 var wins = 0;
 var losses = 0;
 //holds number of underscores
 var underscores = 0;
+var guessesL = 0;
+var counterCorrGuesses = 0;
 var lettersinAnim = [];
 var userGuess = [];
 var wGuesses = [];
 //holds underscores and correct guesses
 var CorrGuessesandUnders = [];
-var guessesL = 0;
-var counterCorrGuesses = 0;
+
 
 function gameStart () { 
     //computer chooses word from animal array randomly when prompted by first event listener
@@ -21,23 +22,28 @@ function gameStart () {
     var randAnim = AusAnimals [Math.floor(Math.random() * AusAnimals.length)];
     console.log('random word: ' + randAnim);
     //splits the randomly chosen word into array of letters
-    var lettersinAnim = randAnim.split('');
+    //var lettersinAnim = randAnim.split('');
     //get the number of underscores/word-blanks
-    underscores = lettersinAnim.length;
+    //underscores = lettersinAnim.length;
+   //console.log(lettersinAnim)
+   //console.log(underscores)
     //change underscore 'Word to Guess' to match word from animal array chosen randomly (id="word-blanks")
     
     //set beginning of game wrong guesses and guesses left
     wGuesses = [];
     guessesL = 10;
+    CorrGuessesandUnders = [];
     
-    for(var i = 0; i< underscores; i++)
-    {
+    for(var i = 0; i< randAnim.length; i++){
         CorrGuessesandUnders.push('_');
-        document.getElementById('word-blanks').innerHTML = CorrGuessesandUnders;
-      }
-      document.getElementById('word-blanks').innerHTML = CorrGuessesandUnders.join(" ");
-      document.getElementById("guesses-left").innerHTML = guessesL
-  }
+    }
+    
+    document.getElementById('word-blanks').innerHTML = CorrGuessesandUnders.join(' ')
+    document.getElementById('guesses-left').innerHTML = guessesL
+    document.getElementById('wrong-guesses').innerHTML = wGuesses
+    document.getElementById('win-counter').innerHTML = wins
+    document.getElementById('loss-counter').innerHTML = losses
+}
   
 
 function validateInput(userGuess) {
@@ -60,77 +66,68 @@ function validateInput(userGuess) {
 
 
 function updateScore(){
-    if (updateWin()){ 
-        wins++;
-        document.getElementById("win-counter").innerHTML = wins; 
-        gameStart()  
+    if (CorrGuessesandUnders.indexOf('_') === -1){
+        updateWin();
+    }
 
-}
-else (updateLoss());{
-    
-        losses++;
-        document.getElementById("loss-conter").innerHTML = losses;
-        gameStart()
-        
-        
-
+    else if (guessesL === 0){
+        updateLoss();
     }
 }
 
-function updateWin (underscores = lettersinAnim.length){
-    //document.getElementById("word-blanks").innerHTML = clear "word-blanks"
+function updateWin (){
+    wins++
+    //prints message to board
+    document.getElementById('msg').textContent = 'Winner!';
     gameStart();
     
 
 }
 
-function updateLoss(guessesL= 0) {
-   
+function updateLoss() {
+    losses++
+    //print message to board
+    document.getElementById('msg').textContent = 'Loser';
     gameStart();
     
 }
 
-
-
-
-
-
-
- 
 
 //get/store input from user
-document.onkeyup = function compareGuess(event)
-{
-    userGuess = event.key;
+document.onkeyup = function compareGuess(event) {
+    var userGuess = event.key;
     console.log (userGuess)
     
-    if (randAnim.indexOf(userGuess) > -1) {
-         
-        for (var j=0; j<randAnim.length; j++) {
-            if (randAnim[j] === userGuess) {
-                CorrGuessesandUnders[j] = userGuess;
-                document.getElementById("word-blanks").innerHTML = CorrGuessesandUnders
-               // updateScore()
+    if (validateInput(userGuess)) {
+        //this whole second if statement is not working; all letters being passed to else and printed as wrong guesses 
+        if (randAnim.indexOf(userGuess) > -1) {
+            for (var j = 0; j < randAnim.length; j++) {
+                if (randAnim[j] === userGuess) {
+                    CorrGuessesandUnders[j] = userGuess; 
                 
-            }}
-        }
+                }
+            }
+            document.getElementById('word-blanks').innerHTML = CorrGuessesandUnders;
 
-        else {//??this else function worked when I had a different function for the if??
+        } 
+        
+        else {
             wGuesses.push(userGuess);
             guessesL--;
-            document.getElementById("guesses-left").innerHTML = guessesL;
-            document.getElementById("wrong-guesses").innerHTML = wGuesses;
-            console.log ('wrong guesses:' + wGuesses);
-            //updateScore();
-            
-          }
+            }
+            document.getElementById('word-blanks').innerHTML = CorrGuessesandUnders.join(' ');
+            document.getElementById('guesses-left').innerHTML = guessesL
+            document.getElementById('wrong-guesses').innerHTML = wGuesses
+            document.getElementById('win-counter').innerHTML = wins
+            document.getElementById('loss-counter').innerHTML = losses
 
-          updateScore()
+            updateScore()
+        }
+
+    }
           
-          
-            
+    gameStart ();
 
-            gameStart ();
 
 
     
@@ -149,4 +146,3 @@ document.onkeyup = function compareGuess(event)
 
 
 
-}
